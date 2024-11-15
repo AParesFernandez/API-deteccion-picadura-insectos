12fa1dfa827985e3edf3ee91aeb5f2cb84b311e7
AParesFernandez <amandapares87@gmail.com>
2024-11-15 17:44:51 -0300

    intento 3.4

--- app.py
+++ app.py
@@ -77,5 +77,4 @@ def predict_webcam():
     
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado o 5000 por defecto
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
